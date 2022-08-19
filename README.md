18c4484bbab9b92ce56c6243b15882d45881f829
Kat <kamilne39@gmail.com>
2022-08-19 18:42:45 +0100

    first codewar attempt using git

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Coding-Challenges
+# Code Wars: Coding-Challenges
---
+++ javaScript/8kyu/multiply.js
@@ -0,0 +1,13 @@
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//     a * b
+//   }
+  
+//My solution
+function multiply(a, b){
+    return a * b
+  }
+
+//Other clever/better solutions
+multiply = (a, b) => a * b;
